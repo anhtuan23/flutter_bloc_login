a53fbf61d818c5c19d8341398729a29e7da69a6b
Do Tuan Anh <dotuananh.dta@gmail.com>
2020-07-01 14:31:53 +0700

    add AuthenticationBloc implementation

--- lib/bloc/authentication_bloc.dart
+++ lib/bloc/authentication_bloc.dart
@@ -2,12 +2,19 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc_login/repositories/user_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'authentication_event.dart';
 part 'authentication_state.dart';
 
-class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> {
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final UserRepository userRepository;
+
+  AuthenticationBloc({@required this.userRepository})
+      : assert(userRepository != null);
+
   @override
   AuthenticationState get initialState => AuthenticationInitial();
 
@@ -15,6 +22,26 @@ class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> 
   Stream<AuthenticationState> mapEventToState(
     AuthenticationEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is AuthenticationStarted) {
+      final bool hasToken = await userRepository.hasToken();
+
+      if (hasToken) {
+        yield AuthenticationSuccess();
+      } else {
+        yield AuthenticationFailure();
+      }
+    }
+
+    if (event is AuthenticationLoggedIn) {
+      yield AuthenticationInProgress();
+      await userRepository.persistToken(event.token);
+      yield AuthenticationSuccess();
+    }
+
+    if (event is AuthenticationLoggedOut) {
+      yield AuthenticationInProgress();
+      await userRepository.deleteToken();
+      yield AuthenticationFailure();
+    }
   }
 }
